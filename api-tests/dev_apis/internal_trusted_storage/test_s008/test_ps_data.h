5f1b8cf9d31bd875c19abca0a765d82ad9616676
cherat01 <chetan.rathore@arm.com>
2019-02-14 15:32:59 +0530

    Reverting constant read buffer & MAX_UINT32 len check
    Change-Id: I58f1a300426db6067687aa418474d38a646e8ae5

--- api-tests/dev_apis/internal_trusted_storage/test_s008/test_ps_data.h
+++ api-tests/dev_apis/internal_trusted_storage/test_s008/test_ps_data.h
@@ -68,9 +68,6 @@ static const test_data s008_data[] = {
  VAL_PS_GET, PSA_PS_SUCCESS /* Call get API with offset = MAX_UINT32 */
 },
 {
- VAL_PS_GET, PSA_PS_SUCCESS /* Call get API with length = MAX_UINT32 */
-},
-{
  VAL_PS_REMOVE, PSA_PS_SUCCESS /* Remove the storage entity */
 },
 };
