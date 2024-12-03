b50fcf3475a26eb94b317df93509746c0e7e5573
Ahmad EL JOUAID <ahmad.eljouaid@st.com>
2024-12-03 15:48:42 +0100

    ST : Add STM32WBA platform to psa-arch-tests.
    Change-Id: Ic7947b7f0cc1d922295f6a21539915e402229c9f
    Signed-off-by: Ahmad EL JOUAID <ahmad.eljouaid@st.com>

---
+++ api-tests/platform/targets/tgt_dev_apis_tfm_stm32wba/target_database.h
@@ -0,0 +1,73 @@
+/** @file
+ * Copyright (c) 2025, Arm Limited or its affiliates. All rights reserved.
+ * SPDX-License-Identifier : Apache-2.0
+ *
+ * Copyright (c) 2025, STMicroelectronics.
+ * SPDX-License-Identifier : Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+#ifndef _TARGET_DATABASE_H_
+#define _TARGET_DATABASE_H_
+static const uint32_t target_database[] = {
+0x54425341,
+0x5f434647,
+0x7467745f,
+0x5f434647,
+0x00000001,
+0x000000b8,
+0x01000000,
+0x0000007c,
+0x00000002,
+0x01010000,
+0x01000038,
+0x00000000,
+0x00000000,
+0x40013800,
+0x00000fff,
+0x000000ff,
+0x00000012,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x01030000,
+0x01000038,
+0x00000000,
+0x00000000,
+0x40002c00,
+0x00000fff,
+0x000000ff,
+0x00000012,
+0x000f4240,
+0x001e8480,
+0x004c4b40,
+0x01312d00,
+0x00000003,
+0x00000000,
+0x02000000,
+0x00000024,
+0x00000001,
+0x02020000,
+0x01000018,
+0x20000000,
+0x20000fff,
+0x00000000,
+0x00000012,
+0xffffffff
+};
+
+#endif
